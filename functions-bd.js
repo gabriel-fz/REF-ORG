38d33ba2faca0b9457e9aa80fb4250c712526242
Gabriel Fiorese <gabrielfzancanela@gmail.com>
2019-06-21 17:45:06 -0300

    Função de editar citação concluída

--- functions-bd.js
+++ functions-bd.js
@@ -4,43 +4,43 @@ const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 //---------Funções para a tela de bibliografia---------
-function listaBibliografia(){
-    const refs = db.get('referencias').sortBy('text').value()
-    let b = document.querySelector('div#bibliografia')
-    let mostraReferencias = ''
-    for (let contador in refs) {
-        mostraReferencias += `<p class="text-justify ref-bib"> ${refs[contador].text} </p>`
-    } 
-    b.innerHTML = mostraReferencias
+function listaBibliografia() {
+  const refs = db.get('referencias').sortBy('text').value()
+  let b = document.querySelector('div#bibliografia')
+  let mostraReferencias = ''
+  for (let contador in refs) {
+    mostraReferencias += `<p class="text-justify ref-bib"> ${refs[contador].text} </p>`
+  }
+  b.innerHTML = mostraReferencias
 }
 
 //---------Funções para a tela de referências bibliográficas---------
 
-function adicionarReferencias(){
+function adicionarReferencias() {
   let txta = document.querySelector('textarea#txtReferencia').value
-  txta ?  db.get('referencias').push({id: `${db.get('referencias').size().value()+1}`, text: `${txta}`}).write()
-  : alert("Campo não preenchido")
+  txta ? db.get('referencias').push({ id: `${db.get('referencias').size().value() + 1}`, text: `${txta}` }).write()
+    : alert("Campo não preenchido")
 }
 
-function editarReferencias(idRef){
+function editarReferencias(idRef) {
   let txta = document.querySelector(`textarea#novaReferencia${idRef}`).value
-  txta  ? db.get('referencias').find({id: `${idRef}`}).assign({text: `${txta}`}).write()
-  : alert("Campo não preenchido")
+  txta ? db.get('referencias').find({ id: `${idRef}` }).assign({ text: `${txta}` }).write()
+    : alert("Campo não preenchido")
 }
 
-function excluirReferencias(idRef){
+function excluirReferencias(idRef) {
   decisao = confirm("Você deseja mesmo deletar esta citação?")
-  if(decisao){
-    db.get('referencias').remove({id: `${idRef}`}).write()
+  if (decisao) {
+    db.get('referencias').remove({ id: `${idRef}` }).write()
   }
 }
 
-function listaReferencias(){
+function listaReferencias() {
   const refs = db.get('referencias').value()
   let r = document.querySelector('div#referencias')
   let mostraReferencias = ''
   for (let contador in refs) {
-      mostraReferencias += `<div class="card my-2">
+    mostraReferencias += `<div class="card my-2">
                               <div class="row no-gutters">
                                 <div class="col-2 texto-centro">
                                   <button class="btn btn-two btn-lg mx-2" data-toggle="modal" data-target="#Modal${refs[contador].id}"><i class="fas fa-pen fa-sm"></i></button>
@@ -58,67 +58,98 @@ function listaReferencias(){
                             </div>
                             
                             <div class="modal fade" id="Modal${refs[contador].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-lg">
-                              <div class="modal-content">
-                                <div class="modal-header">
-                                  <h5 class="modal-title">Editar</h5>
-                                </div>
-                                <div class="modal-body">
-                                  <textarea class="form-control mt-2" id="novaReferencia${refs[contador].id}" rows="3">${refs[contador].text}</textarea>
-                                </div>
-                                <div class="modal-footer">
-                                  <button type="button" class="btn btn-one" onclick="editarReferencias(${refs[contador].id})">Salvar</button>
-                                  <button type="button" class="btn btn-one" data-dismiss="modal">Cancelar</button>
+                              <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title">Editar</h5>
+                                  </div>
+                                  <div class="modal-body">
+                                    <textarea class="form-control mt-2" id="novaReferencia${refs[contador].id}" rows="3">${refs[contador].text}</textarea>
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-one" onclick="editarReferencias(${refs[contador].id})">Salvar</button>
+                                    <button type="button" class="btn btn-one" data-dismiss="modal">Cancelar</button>
+                                  </div>
                                 </div>
                               </div>
-                            </div>
-                          </div>`
+                            </div>`
   }
   r.innerHTML = mostraReferencias
 }
 
 //---------Funções para a tela de citações---------
 
-function listaCitacoes(){
+function adicionarCitacoes() {
+  let txtar = document.querySelector('textarea#txtReferenciac').value
+  let txtac = document.querySelector('textarea#txtCitacao').value
+  txtar && txtac ? db.get('citacoes').push({ id: `${db.get('citacoes').size().value() + 1}`, textCitacao: `${txtac}`, textReferencia: `${txtar}` }).write()
+    : alert("Campos não preenchidos")
+}
+
+function editarReferencias(idCit) {
+  let txtar = document.querySelector(`textarea#txtReferenciac${idCit}`).value
+  let txtac = document.querySelector(`textarea#txtCitacao${idCit}`).value
+  txtar && txtac ? db.get('citacoes').find({ id: `${idCit}` }).assign({ textCitacao: `${txtac}`, textReferencia: `${txtar}` }).write()
+    : alert("Campos não preenchidos")
+}
+
+function excluirCitacoes(idCit) {
+  decisao = confirm("Você deseja mesmo deletar esta citação?")
+  if (decisao) {
+    db.get('citacoes').remove({ id: `${idCit}` }).write()
+  }
+}
+
+function listaCitacoes() {
   const cits = db.get('citacoes').value()
   let c = document.querySelector('div#citacoes')
   let mostraCitacoes = ''
   for (let contador in cits) {
-      mostraCitacoes += `<div class="card my-2">
-                              <div class="row no-gutters">
-                                <div class="col-2 texto-centro">
-                                  <button class="btn btn-two btn-lg mx-2" data-toggle="modal" ><i class="fas fa-pen fa-sm"></i></button>
-                                  <button class="btn btn-two btn-lg mx-2" onclick="excluirCitacoes(${cits[contador].id})"><i class="fas fa-trash-alt fa-sm"></i></button>
-                                </div>
-                                <div class="col-10">
-                                  <div class="card-body">
-                                    <div id="Citacao"></div>
-                                    <p class="card-text">
-                                      ${cits[contador].textCitacao}
-                                    </p>
-                                    <div class="dropdown-divider"></div>
-                                    <div id="referenciaCitacao"></div>
-                                    <p class="card-text">
-                                      ${cits[contador].textReferencia}
-                                    </p>
-                                  </div>
+    mostraCitacoes += `<div class="card my-2">
+                            <div class="row no-gutters">
+                              <div class="col-2 texto-centro">
+                                <button class="btn btn-two btn-lg mx-2" data-toggle="modal" data-target="#Modal${cits[contador].id}"><i class="fas fa-pen fa-sm"></i></button>
+                                <button class="btn btn-two btn-lg mx-2" onclick="excluirCitacoes(${cits[contador].id})"><i class="fas fa-trash-alt fa-sm"></i></button>
+                              </div>
+                              <div class="col-10">
+                                <div class="card-body">
+                                  <div id="Citacao"></div>
+                                  <p class="card-text">
+                                    ${cits[contador].textCitacao}
+                                  </p>
+                                  <div class="dropdown-divider"></div>
+                                  <div id="referenciaCitacao"></div>
+                                  <p class="card-text">
+                                    ${cits[contador].textReferencia}
+                                  </p>
                                 </div>
                               </div>
-                            </div>`
+                            </div>
+                          </div>
+                          
+                          <div class="modal fade" id="Modal${cits[contador].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5 class="modal-title">Editar</h5>
+                                </div>
+                                <div class="modal-body">
+                                  <form class="text-center">
+                                    <div class="form-group">
+                                      <textarea class="form-control mt-2" id="txtReferenciac${cits[contador].id}" rows="3">${cits[contador].textCitacao}</textarea>
+                                    </div>
+                                    <div class="form-group">
+                                      <textarea class="form-control mt-2" id="txtCitacao${cits[contador].id}" rows="2">${cits[contador].textReferencia}</textarea>
+                                    </div>
+                                  </form>
+                                </div>
+                                <div class="modal-footer">
+                                  <button type="button" class="btn btn-one" onclick="editarReferencias(${cits[contador].id})">Salvar</button>
+                                  <button type="button" class="btn btn-one" data-dismiss="modal">Cancelar</button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>`
   }
   c.innerHTML = mostraCitacoes
-}
-
-function adicionarCitacoes(){
-  let txtar = document.querySelector('textarea#txtReferenciac').value
-  let txtac = document.querySelector('textarea#txtCitacao').value
-  txtar && txtac ?  db.get('citacoes').push({id: `${db.get('citacoes').size().value()+1}`, textCitacao: `${txtac}`, textReferencia: `${txtar}`}).write()
-  : alert("Campos não preenchidos")
-}
-
-function excluirCitacoes(idCit){
-  decisao = confirm("Você deseja mesmo deletar esta citação?")
-  if(decisao){
-    db.get('citacoes').remove({id: `${idCit}`}).write()
-  }
 }
