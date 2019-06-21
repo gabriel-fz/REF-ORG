4d6a6b6ce94f8678265bd7d2604104bc62eea206
Gabriel Fiorese <gabrielfzancanela@gmail.com>
2019-06-21 13:56:13 -0300

    Adicionando referências no banco de dados

--- functions-bd.js
+++ functions-bd.js
@@ -3,29 +3,48 @@ const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 
-const refs = db.get('referencias').sortBy('text').value()
-
-
+//---------Funções para a tela de bibliografia---------
 function listBibliografia(){
+    const refs = db.get('referencias').sortBy('text').value()
     let b = document.querySelector('div#bibliografia')
     let mostraReferencias = ''
     for (let contador in refs) {
-        mostraReferencias += '<p class="text-justify ref-bib">'
-          mostraReferencias += refs[contador].text
-        mostraReferencias += '</p>'
-    }
-
+        mostraReferencias += `<p class="text-justify ref-bib"> ${refs[contador].text} </p>`
+    } 
     b.innerHTML = mostraReferencias
 }
 
+//---------Funções para a tela de referências bibliográficas---------
+
+function adicionarReferencias(){
+  let a = document.querySelector('textarea#txtReferencia').value
+  db.get('referencias').push({
+    id: `${db.get('referencias').size().value()+1}`,
+    text: `${a}`
+  }).write()
+}
+
 function listReferencias(){
-  let b = document.querySelector('div#bibliografia')
+  const refs = db.get('referencias').value()
+  let b = document.querySelector('div#referencias')
   let mostraReferencias = ''
   for (let contador in refs) {
-      mostraReferencias += '<p class="text-justify">'
-        mostraReferencias += refs[contador].text
-      mostraReferencias += '</p>'
+      mostraReferencias += `<div class="card my-2">
+                              <div class="row no-gutters">
+                                <div class="col-2 texto-centro">
+                                  <button class="btn btn-two btn-lg mx-2"><i class="fas fa-pen fa-sm"></i></button>
+                                  <button class="btn btn-two btn-lg mx-2" onclick="excluir()"><i
+                                      class="fas fa-trash-alt fa-sm"></i></button>
+                                </div>
+                                <div class="col-10">
+                                  <div class="card-body">
+                                    <p class="card-text">
+                                      ${refs[contador].text}
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>`
   }
-
   b.innerHTML = mostraReferencias
 }
